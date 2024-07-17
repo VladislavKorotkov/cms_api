3722258e6a27ea90294ca2c4da10236a28b766be
VladislavKortokov <vladislav.korotkow@gmail.com>
2024-07-18 00:10:41 +0300

    add models

---
+++ src/main/java/com/korotkov/cms/models/Post.java
@@ -0,0 +1,27 @@
+package com.korotkov.cms.models;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String content;
+    private LocalDateTime created_at;
+    private LocalDateTime updated_at;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}
---
+++ src/main/java/com/korotkov/cms/models/User.java
@@ -0,0 +1,23 @@
+package com.korotkov.cms.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String email;
+
+    @OneToMany(mappedBy = "user")
+    private List<Post> posts;
+}
